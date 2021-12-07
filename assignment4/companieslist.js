d0746aaeb065d40f7adb8f0fb36f54e250497ae5
Prithiviraj69 <85940424+Prithiviraj69@users.noreply.github.com>
2021-12-08 00:27:53 +0530

    Add files via upload

---
+++ assignment4/companieslist.js
@@ -0,0 +1,23 @@
+const company = [{
+        "companyid": "c001",
+        "name": "parkson",
+        "productid": "p101,p105"
+    },
+    {
+        "companyid": "c002",
+        "name": "Welspun",
+        "productid": "p102"
+    },
+    {
+        "companyid": "c003",
+        "name": "Atul",
+        "productid": "p103"
+    },
+    {
+        "companyid": "c004",
+        "name": "Lykaa",
+        "productid": "p104"
+    }
+];
+
+module.exports = company;
---
+++ assignment4/productslist.js
@@ -0,0 +1,43 @@
+const prod = [{
+        "productid": "p101",
+        "title": "Drangon Ball Z",
+        "price": "1500",
+        "category": "pc-game",
+        "companyid": "c001",
+        "sellerid": "seller8055"
+    },
+    {
+        "productid": "p102",
+        "title": "Naruto",
+        "price": "1200",
+        "category": "pc-game",
+        "companyid": "c002",
+        "sellerid": "seller8055"
+    },
+    {
+        "productid": "p103",
+        "title": "Attack of Titans",
+        "price": "1200",
+        "category": "ps4-game",
+        "companyid": "c003",
+        "sellerid": "gamesworld77"
+    },
+    {
+        "productid": "p104",
+        "title": "One Piece",
+        "price": "800",
+        "category": "pc-game",
+        "companyid": "c004",
+        "sellerid": "gamesworld77"
+    },
+    {
+        "productid": "p105",
+        "title": "Death Note",
+        "price": "2500",
+        "category": "ps4-game",
+        "companyid": "c001",
+        "sellerid": "seller8055"
+    },
+];
+
+module.exports = prod;
